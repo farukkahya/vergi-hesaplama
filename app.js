91e0610b1a8bee2cf6cb050745832a7a515d5e72
farukkahya <farukkahya55@gmail.com>
2023-07-09 15:05:17 +0300

    the code has been simplified and comments added.

--- app.js
+++ app.js
@@ -6,41 +6,37 @@ const vergiSpan = document.getElementById('vergi');
 const genelToplam = document.getElementById('genel-toplam');
 let vergiOrani;
 
+// Varsayılan değerler atandı
 btnRadio1.checked = true;
-vergiOraniInput.value = '%1';
+vergiOraniInput.value = 1;
 vergiOraniInput.disabled = true;
 vergiOrani = 1;
+tutarInput.focus();
 
+// Vergi oranını alıp vergi tutarını hesaplayan fonksiyon
 // (Net tutar) x (1+ vergi oranı/100)
 const calculate = (netTutar, vergiOrani) => {
 	return netTutar * (1 + vergiOrani / 100);
 }
 
-
+// radio buttonların seçili olup olmadığını kontrol ederek inputları dolduran fonksiyon
 const isChecked = () => {
 	let radioButtons = document.getElementsByName('vergi-oran');
 	for (let i = 0; i < radioButtons.length; i++) {
 		if (radioButtons[i].checked && radioButtons[i].id !== 'btnradiofree') {
-			if (radioButtons[i].value === '1') {
-				vergiOraniInput.value = '%1';
-				vergiOrani = 1;
-			} else if (radioButtons[i].value === '8') {
-				vergiOraniInput.value = '%8';
-				vergiOrani = 8;
-			} else if (radioButtons[i].value === '18') {
-				vergiOraniInput.value = '%18';
-				vergiOrani = 18;
-			}
+			vergiOraniInput.value = radioButtons[i].value;
 			vergiOraniInput.disabled = true;
+			tutarInput.focus();
 		} else if (radioButtons[i].checked && radioButtons[i].id === 'btnradiofree') {
-			vergiOraniInput.value = '';
 			vergiOraniInput.disabled = false;
 		}
 	}
 }
 
+// inputların değerlerini kontrol ederek hesaplanan değerleri göstermek için keyup eventi eklenmiştir
 tutarInput.addEventListener('keyup', (e) => {
-	if (e.key === 'Enter') {
+	if (e.key === 'Enter')/* Basılan tuş Enter ise*/ {
+		vergiOrani = Number(vergiOraniInput.value);
 		araToplam.innerHTML = tutarInput.value;
 		vergiSpan.innerHTML = `${calculate(tutarInput.value, vergiOrani) - (tutarInput.value)}`
 		genelToplam.innerHTML = calculate(tutarInput.value, vergiOrani);
